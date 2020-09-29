453122ae6746f3d8990401791483901f25b4725f
Danila <Ltdfm@mail.ru>
2020-09-29 13:51:12 +0300

    Создаёт и рендерит компонент главной страницы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
 
+const Setting = {
+  PLACES_COUNT: 312
+};
+
+
+ReactDOM.render(
+    <App
+      placesCount={Setting.PLACES_COUNT}
+    />,
+    document.querySelector(`#root`)
+);
