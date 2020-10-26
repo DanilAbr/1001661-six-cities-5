82cd7d76b8b2eb0456b9aed163d31eaff355339b
Danila <Ltdfm@mail.ru>
2020-10-26 23:38:59 +0300

    Переводит компонент offer-card в функциональный

--- src/components/offer-card/offer-card.jsx
+++ src/components/offer-card/offer-card.jsx
@@ -1,80 +1,68 @@
-import React, {PureComponent} from 'react';
-import PropTypes from 'prop-types';
-import offerProp from './offer.prop';
-import {offerClasses} from "../../utils/const";
-import {ActionCreator} from '../../store/action';
+import React from 'react';
 import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {offerClasses} from '../../utils/const';
+import {ActionCreator} from '../../store/action';
+import offerProp from './offer.prop';
 
 
-class OfferCard extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
+const OfferCard = (props) => {
+  const {offer, onOfferCardClick, type, onCardHover, onCardDeHover} = props;
+  const isFavorites = type === `isFavorites`;
 
-  render() {
-    const {
-      offer,
-      onOfferCardClick,
-      type,
-      onCardHover,
-      onCardDeHover
-    } = this.props;
-    const isFavorites = type === `isFavorites`;
-
-    return (
-      <article
-        className={offerClasses[type].article}
-        onMouseEnter={() => onCardHover(offer.id)}
-        onMouseLeave={() => onCardDeHover()}
-        onClick={onOfferCardClick}
-      >
-        {!isFavorites && offer.isPremium &&
-          <div className="place-card__mark">
-            <span>Premium</span>
-          </div>
-        }
-        <div className={offerClasses[type].imageWrap}>
-          <a href="#">
-            <img
-              className="place-card__image"
-              src={offer.images[0]}
-              width={isFavorites ? `150` : `260`}
-              height={isFavorites ? `110` : `150`}
-              alt="Place image"
-            />
-          </a>
+  return (
+    <article
+      className={offerClasses[type].article}
+      onMouseEnter={() => onCardHover(offer.id)}
+      onMouseLeave={() => onCardDeHover()}
+      onClick={onOfferCardClick}
+    >
+      {!isFavorites && offer.isPremium &&
+        <div className="place-card__mark">
+          <span>Premium</span>
         </div>
-        <div className={offerClasses[type].infoWrap}>
-          <div className="place-card__price-wrapper">
-            <div className="place-card__price">
-              <b className="place-card__price-value">&euro;{offer.price}</b>
-              <span className="place-card__price-text">&#47;&nbsp;night</span>
-            </div>
-            <button
-              className={offerClasses[type].button}
-              type="button"
-            >
-              <svg className="place-card__bookmark-icon" width="18" height="19">
-                <use xlinkHref="#icon-bookmark"/>
-              </svg>
-              <span className="visually-hidden">To bookmarks</span>
-            </button>
+      }
+      <div className={offerClasses[type].imageWrap}>
+        <a href="#">
+          <img
+            className="place-card__image"
+            src={offer.images[0]}
+            width={isFavorites ? `150` : `260`}
+            height={isFavorites ? `110` : `150`}
+            alt="Place image"
+          />
+        </a>
+      </div>
+      <div className={offerClasses[type].infoWrap}>
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{offer.price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <div className="place-card__rating rating">
-            <div className="place-card__stars rating__stars">
-              <span style={{width: offer.rating * 20 + `%`}}/>
-              <span className="visually-hidden">Rating</span>
-            </div>
-          </div>
-          <h2 className="place-card__name">
-            <a href="#">{offer.title}</a>
-          </h2>
-          <p className="place-card__type">{offer.type}</p>
+          <button
+            className={offerClasses[type].button}
+            type="button"
+          >
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"/>
+            </svg>
+            <span className="visually-hidden">To bookmarks</span>
+          </button>
         </div>
-      </article>
-    );
-  }
-}
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{width: offer.rating * 20 + `%`}}/>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">{offer.title}</a>
+        </h2>
+        <p className="place-card__type">{offer.type}</p>
+      </div>
+    </article>
+  );
+};
 
 
 OfferCard.propTypes = {
@@ -92,7 +80,7 @@ const mapDispatchToProps = (dispatch) => ({
   },
   onCardDeHover: () => {
     dispatch(ActionCreator.resetCard());
-  }
+  },
 });
 
 
